a68f7fd1fa8fabbde3b2106f36f10ec306ef7255
YJ Kim <yj@YJs-MacBook-Pro.lan>
2018-11-30 18:23:50 +1300

    add merge sort logic

--- src/index.js
+++ src/index.js
@@ -68,7 +68,19 @@ class Sorting extends React.Component {
   }
 
   bubbleSort() {
-    alert("bubbleSort");
+    const array = this.state.array.slice();
+    for( let i=0;i<array.length-1; i++){
+      for( let j=0; j<array.length-i-1; j++){
+        if (array[j] > array[j+1]){
+          let temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+        }
+      }
+    }
+    this.setState({
+      array: array,
+    })
   }
 
   mergeSort() {
