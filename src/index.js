95f923e0d5cf555a7accc521a994eeff003ffb44
YJ Kim <yj@wifi-172-23-77-209.uoa-wifi.auckland.ac.nz>
2018-11-30 14:49:17 +1300

    structure done

--- src/index.js
+++ src/index.js
@@ -3,24 +3,60 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-
+  render() {
+    return(
+      <button>1</button>
+    )
+  }
 }
 
 class Array extends React.Component {
-
+  render() {
+    return(
+      <div>
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    )
+  }
 }
 
 class ControlPanel extends React.Component {
+  render() {
+    return(
+      'test'
+    )
+  }
 
 }
 
 class Sorting extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <div>
+          <Array />
+        </div>
+        <div>
+          <Button name={"Mix"}/>
+          <Button name={"Mix"}/><Button name={"Mix"}/>
+        </div>
+      </div>
     )
   }
+}
 
+function Button(props) {
+  return(
+    <button>{props.name}</button>
+  )
 }
 
 ReactDOM.render(<Sorting />, document.getElementById("root"));
