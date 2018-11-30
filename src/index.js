dbbd8bf1ed493933c9c45cd2e0a5faa904de82ff
YJ Kim <yj@wifi-172-23-77-209.uoa-wifi.auckland.ac.nz>
2018-11-30 15:42:01 +1300

    generate random numbers

--- src/index.js
+++ src/index.js
@@ -4,59 +4,94 @@ import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return(
-      <button>1</button>
-    )
+    return <button>{this.props.value}</button>;
   }
 }
 
 class Array extends React.Component {
   render() {
-    return(
+    return (
       <div>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        <Square value={this.props.array[0]}/>
+        <Square value={this.props.array[1]}/>
+        <Square value={this.props.array[2]}/>
+        <Square value={this.props.array[3]}/>
+        <Square value={this.props.array[4]}/>
+        <Square value={this.props.array[5]}/>
+        <Square value={this.props.array[6]}/>
+        <Square value={this.props.array[7]}/>
+        <Square value={this.props.array[8]}/>
+        <Square value={this.props.array[9]}/>
       </div>
-    )
+    );
   }
 }
 
 class ControlPanel extends React.Component {
   render() {
-    return(
-      'test'
-    )
+    return "test";
   }
-
 }
 
 class Sorting extends React.Component {
+  state = {
+    array: [null, null, null, null, null, null, null, null, null, null]
+  };
+
+  genNumbers(array) {
+    const unsortedArray = array.slice();
+
+    for (let i = 0; i < unsortedArray.length; i++) {
+      unsortedArray[i] = Math.round(Math.random() * 9);
+    }
+
+    this.setState({
+      array: unsortedArray
+    });
+  }
+
+  bubbleSort(array) {
+    alert("bubbleSort");
+  }
+
+  mergeSort(array) {
+    alert("MergeSort");
+  }
+
   render() {
+    console.log(this.state.array);
     return (
       <div>
         <div>
-          <Array />
+          <Array array={this.state.array}/>
         </div>
         <div>
-          <Button name={"Mix"}/>
-          <Button name={"Mix"}/><Button name={"Mix"}/>
+          <div>
+            <Button
+              name={"Mix"}
+              onClick={() => this.genNumbers(this.state.array)}
+            />
+          </div>
+          <div>
+            <Button
+              name={"Bubble Sort"}
+              onClick={() => this.bubbleSort(this.state.array)}
+            />
+            <Button
+              name={"Merge Sort"}
+              onClick={() => this.mergeSort(this.state.array)}
+            />
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-function Button(props) {
-  return(
-    <button>{props.name}</button>
-  )
+class Button extends React.Component {
+  render() {
+    return <button onClick={this.props.onClick}>{this.props.name}</button>;
+  }
 }
 
 ReactDOM.render(<Sorting />, document.getElementById("root"));
