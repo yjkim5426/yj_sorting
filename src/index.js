7c98ba7f84b438053cf749210dbb90b07e6330a3
YJ Kim <yj@wifi-172-23-77-209.uoa-wifi.auckland.ac.nz>
2018-11-30 14:36:14 +1300

    move index.js and index.css files to src folder

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Square extends React.Component {
+
+}
+
+class Array extends React.Component {
+
+}
+
+class ControlPanel extends React.Component {
+
+}
+
+class Sorting extends React.Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+
+}
+
+ReactDOM.render(<Sorting />, document.getElementById("root"));
